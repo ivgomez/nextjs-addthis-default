0be3b6ed6b1c47c5659f571a372d4448f784c235
ivgomez <iv.gomez@hotmail.com>
2022-01-31 18:26:57 -0500

    remove imports

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 import ShareComponent from "../components/ShareComponent";
 
 export default function Home() {
