562218c7c02ee9dc6dc2e4d92936c77048b69c72
ivgomez <iv.gomez@hotmail.com>
2022-02-11 20:21:41 -0500

    addint new libraries

--- components/ShareComponent.jsx
+++ components/ShareComponent.jsx
@@ -7,10 +7,12 @@ import SkypeShareLink from "../pages/images/SkypeShareLink";
 import FacebookShareLink from "../pages/images/FacebookShareLink";
 import MessengerShareLink from "../pages/images/MessengerShareLink";
 import TelegramShareLink from "../pages/images/TelegramShareLink";
+import SMSShareLink from "../pages/images/SMSShareLink";
 
 function ShareComponent() {
   const [open, setOpen] = useState(false);
   const [openA2A, setOpenA2A] = useState(false);
+  const [openST, setOpenST] = useState(false);
 
   var a2a_config = a2a_config || {};
   a2a_config.templates = a2a_config.templates || {};
@@ -18,107 +20,100 @@ function ShareComponent() {
   const addthisViewEvent = () => {
     window.a2a.init_all();
     if (window?.addthis) {
+      //console.log("addthis", window.addthis);
       window.addthis.toolbox(".addthis_sharing_toolbox");
     }
+    if (window?.a2a) {
+      //console.log("a2a", window.a2a);
+      window.a2a.init_all();
+    }
     if (window?.st) {
-      console.log("object", window.st);
+      console.log("st", window.st);
       window.st.initialize();
     }
-    // if (window.a2a) {
-    //   console.log("object", window);
-    //   window.a2a.init_all();
-    // }
   };
 
   useEffect(() => {
     addthisViewEvent();
     //console.log("object", window);
-  }, [open, openA2A]);
+  }, [open, openA2A, openST]);
 
   return (
     <div style={{ width: "90%", margin: "0 auto" }}>
       <h1>SHARE BUTTONS LIBRARIES - EXAMPLES FOR REACT JS/NEXT.js</h1>
-      {/* <div style={{ padding: "2rem 0" }}>
-        <p>SHARETHIS BUTTON</p>
-        <div className="sharethis-inline-share-buttons"></div>
-      </div> */}
-      <div style={{ display: "block" }}>
-        <h2>AddThis Library - Buttons customized</h2>
-        <button onClick={() => setOpen(!open)}>Simulate modal</button>
-        {open ? (
-          <div style={{ width: "60%", margin: "0 auto" }}>
-            <p>
-              <a
-                href="https://www.addthis.com/"
-                target="_blank"
-                rel="noreferrer"
-                style={{
-                  textDecoration: "underline",
-                  cursor: "pointer",
-                  color: "#10a9d8",
-                }}
-              >
-                AddThis
-              </a>{" "}
-              Library - Share buttons customized{" "}
-            </p>
-            <Modal>
-              <div className="addthis_sharing_toolbox">
-                <div className="custom_images">
-                  <ShareLinksWrapper>
-                    <ShareLinksGroup>
-                      <ShareLinks className="addthis_button_link">
-                        <CopyLink alt="Copy Link" />
-                        <ShareLinksText>COPY LINK</ShareLinksText>
-                      </ShareLinks>
-                      <ShareLinks className="addthis_button_whatsapp">
-                        <WhatsappShareLink alt="Share to Whatsapp" />
-                        <ShareLinksText>WHATSAPP</ShareLinksText>
-                      </ShareLinks>
-                      <ShareLinks className="addthis_button_skype">
-                        <SkypeShareLink alt="Share to Skype" />
-                        <ShareLinksText>SKYPE</ShareLinksText>
-                      </ShareLinks>
-                    </ShareLinksGroup>
-                    <ShareLinksGroup>
-                      <ShareLinks className="addthis_button_facebook">
-                        <FacebookShareLink alt="Share to Facebook" />
-                        <ShareLinksText>FACEBOOK</ShareLinksText>
-                      </ShareLinks>
-                      <ShareLinks
-                        className="addthis_button_messenger"
-                        addthis:url="http://example.com"
-                        addthis:title="An excellent website"
-                      >
-                        <MessengerShareLink />
-                        <ShareLinksText>MESSENGER</ShareLinksText>
-                      </ShareLinks>
-                      <ShareLinks className="addthis_button_telegram">
-                        <TelegramShareLink alt="Share to Telegram" />
-                        <ShareLinksText>TELEGRAM</ShareLinksText>
-                      </ShareLinks>
-                    </ShareLinksGroup>
-                  </ShareLinksWrapper>
-                </div>
-              </div>
 
-              <div style={{ padding: "0 1rem" }}>
-                <h3>ShareThis Library - Tik Tok</h3>
-                <div
-                  className="sharethis-inline-follow-buttons"
-                  data-title="Follow us on Tik Tok"
-                  data-url="@corcorangroup/video/7059864242601823535?is_from_webapp=1&sender_device=pc&web_id7041204526691960325"
-                  style={{ padding: "1rem 0" }}
-                ></div>
-              </div>
-            </Modal>
-          </div>
-        ) : null}
-        <div style={{ display: "none" }}>
+      <div style={{ display: "block" }}>
+        <div style={{ display: "block" }}>
+          <h2>AddThis Library - Buttons customized</h2>
+          <button onClick={() => setOpen(!open)}>
+            Simulating a modal open/close
+          </button>
+          {open ? (
+            <div style={{ width: "60%", margin: "0 auto" }}>
+              <p>
+                <a
+                  href="https://www.addthis.com/"
+                  target="_blank"
+                  rel="noreferrer"
+                  style={{
+                    textDecoration: "underline",
+                    cursor: "pointer",
+                    color: "#10a9d8",
+                  }}
+                >
+                  AddThis
+                </a>{" "}
+                Library - Share buttons customized{" "}
+              </p>
+              <Modal>
+                <div className="addthis_sharing_toolbox">
+                  <div className="custom_images" style={{ display: "block" }}>
+                    <ShareLinksWrapper>
+                      <ShareLinksGroup>
+                        <ShareLinks className="addthis_button_link">
+                          <CopyLink alt="Copy Link" />
+                          <ShareLinksText>COPY LINK</ShareLinksText>
+                        </ShareLinks>
+                        <ShareLinks className="addthis_button_whatsapp">
+                          <WhatsappShareLink alt="Share to Whatsapp" />
+                          <ShareLinksText>WHATSAPP</ShareLinksText>
+                        </ShareLinks>
+                        <ShareLinks className="addthis_button_skype">
+                          <SkypeShareLink alt="Share to Skype" />
+                          <ShareLinksText>SKYPE</ShareLinksText>
+                        </ShareLinks>
+                      </ShareLinksGroup>
+                      <ShareLinksGroup>
+                        <ShareLinks className="addthis_button_facebook">
+                          <FacebookShareLink alt="Share to Facebook" />
+                          <ShareLinksText>FACEBOOK</ShareLinksText>
+                        </ShareLinks>
+                        <ShareLinks
+                          className="addthis_button_messenger"
+                          addthis:url="http://example.com"
+                          addthis:title="An excellent website"
+                        >
+                          <MessengerShareLink />
+                          <ShareLinksText>MESSENGER</ShareLinksText>
+                        </ShareLinks>
+                        <ShareLinks className="addthis_button_telegram">
+                          <TelegramShareLink alt="Share to Telegram" />
+                          <ShareLinksText>TELEGRAM</ShareLinksText>
+                        </ShareLinks>
+                      </ShareLinksGroup>
+                    </ShareLinksWrapper>
+                  </div>
+                </div>
+              </Modal>
+            </div>
+          ) : null}
+        </div>
+
+        <div style={{ display: "block" }}>
           <h2>AddToAny Library</h2>
           <div style={{ padding: "1rem 0" }}>
             <button onClick={() => setOpenA2A(!openA2A)}>
-              Open AddToAny Links
+              Simulating a modal open/close
             </button>
 
             {openA2A ? (
@@ -181,145 +176,170 @@ function ShareComponent() {
             ) : null}
           </div>
         </div>
-      </div>
-      <div style={{ padding: "1rem 0" }}>
-        {/* <h2>Default Buttons</h2> */}
-        <div style={{ display: "none" }}>
-          <h3>Addthis Library - Default buttons</h3>
-          <div className="addthis_inline_share_toolbox"></div>
-        </div>
-        <div style={{ padding: "2rem 0", display: "none" }}>
-          <h3>AddToAny Library - Default buttons</h3>
-          <div className="a2a_kit a2a_kit_size_32 a2a_default_style">
-            <a className="a2a_button_sms"></a>
-            <a className="a2a_button_email"></a>
+
+        <div style={{ display: "block" }}>
+          <h2>ShareThis Library</h2>
+          <div style={{ padding: "1rem 0" }}>
+            <button onClick={() => setOpenST(!openST)}>
+              Simulating a modal open/close
+            </button>
+
+            {openST ? (
+              <div style={{ width: "60%", margin: "0 auto" }}>
+                <p>
+                  <a
+                    href="https://sharethis.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    style={{
+                      textDecoration: "underline",
+                      cursor: "pointer",
+                      color: "#10a9d8",
+                    }}
+                  >
+                    ShareThis
+                  </a>{" "}
+                  Library - Share buttons customized{" "}
+                </p>
+                <Modal>
+                  <ShareLinksWrapper>
+                    <ShareLinksGroup>
+                      <ShareThisButton
+                        className="st-custom-button"
+                        data-network="copy"
+                      >
+                        <CopyLink alt="Copy Link" />
+                        <ShareLinksText>COPY LINK</ShareLinksText>
+                      </ShareThisButton>
+                      <ShareThisButton
+                        className="st-custom-button"
+                        data-network="facebook"
+                      >
+                        <FacebookShareLink alt="Share to Facebook" />
+                        <ShareLinksText>FACEBOOK</ShareLinksText>
+                      </ShareThisButton>
+                      <ShareThisButton
+                        className="st-custom-button"
+                        data-network="messenger"
+                      >
+                        <MessengerShareLink />
+                        <ShareLinksText>MESSENGER</ShareLinksText>
+                      </ShareThisButton>
+                    </ShareLinksGroup>
+                    <ShareLinksGroup>
+                      <ShareThisButton
+                        className="st-custom-button"
+                        data-network="whatsapp"
+                      >
+                        <WhatsappShareLink alt="Share to Whatsapp" />
+                        <ShareLinksText>WHATSAPP</ShareLinksText>
+                      </ShareThisButton>
+                      <ShareThisButton
+                        className="st-custom-button"
+                        data-network="telegram"
+                      >
+                        <TelegramShareLink alt="Share to Telegram" />
+                        <ShareLinksText>TELEGRAM</ShareLinksText>
+                      </ShareThisButton>
+                      <ShareThisButton
+                        className="st-custom-button"
+                        data-network="sms"
+                      >
+                        <SMSShareLink alt="Share to SMS" />
+                        <ShareLinksText>SMS</ShareLinksText>
+                      </ShareThisButton>
+                      {/* <TiktokShareButton
+                        className="sharethis-inline-follow-buttons"
+                        data-title="Tiktok"
+                        data-url="@corcorangroup/video/7059864242601823535?is_from_webapp=1&sender_device=pc&web_id7041204526691960325"
+                      /> */}
+                    </ShareLinksGroup>
+                  </ShareLinksWrapper>
+                </Modal>
+              </div>
+            ) : null}
           </div>
         </div>
-        <div style={{ padding: "2rem 1rem", display: "block" }}>
-          <h3>ShareThis Library - Tik Tok</h3>
-          {/* <div className="sharethis-inline-share-buttons"></div> */}
-          <div
-            className="sharethis-inline-follow-buttons"
-            style={{ padding: "1rem 0" }}
-          ></div>
-        </div>
       </div>
-
-      {/* <div style={{ padding: "1rem 0" }}>
-        <button onClick={() => setOpenA2A(!openA2A)}>
-          Add To Anny Library
-        </button>
-        <div>
-          {openA2A ? (
-            <Modal>
-              <p>ADDTOANY share links</p>
-              <div
-                className="a2a_kit a2a_kit_size_32 a2a_default_style"
-                data-a2a-url="http://www.example.com/page.html"
-                data-a2a-title="Corcoran"
-              >
-                <ShareLinksWrapper>
-                  <ShareLinksGroup>
-                    <ShareLinks className="a2a_button_copy_link">
-                      <CopyLink alt="Copy Link" />
-                      <ShareLinksText>COPY LINK</ShareLinksText>
-                    </ShareLinks>
-                    <ShareLinks className="a2a_button_whatsapp">
-                      <WhatsappShareLink alt="Share to Whatsapp" />
-                      <ShareLinksText>WHATSAPP</ShareLinksText>
-                    </ShareLinks>
-                    <ShareLinks className="a2a_button_skype">
-                      <SkypeShareLink alt="Share to Skype" />
-                      <ShareLinksText>SKYPE</ShareLinksText>
-                    </ShareLinks>
-                  </ShareLinksGroup>
-                  <ShareLinksGroup>
-                    <ShareLinks className="a2a_button_facebook">
-                      <FacebookShareLink alt="Share to Facebook" />
-                      <ShareLinksText>FACEBOOK</ShareLinksText>
-                    </ShareLinks>
-                    <ShareLinks className="a2a_button_facebook_messenger">
-                      <MessengerShareLink />
-                      <ShareLinksText>MESSENGER</ShareLinksText>
-                    </ShareLinks>
-
-                    <ShareLinks
-                      className="a2a_button_telegram"
-                      href="https://www.corcoran.com/homes-for-sale/474-middle-line-highway-bridgehampton-ny-11932/6418974/regionId=3?WebID=885241"
-                    >
-                      <TelegramShareLink alt="Share to Telegram" />
-                      <ShareLinksText>TELEGRAM</ShareLinksText>
-                    </ShareLinks>
-                  </ShareLinksGroup>
-                </ShareLinksWrapper>
-              </div>
-            </Modal>
-          ) : null}
-        </div>
-      </div>
-      <hr />
-      <p>API BUTTON 1</p>
-      <div style={{ width: "15rem" }}>
-        <TelegramButton
-          href="https://xn--r1a.link/share/url?url=https%3A%2F%2Fnextjs-addthis-k70scbjjn-ivgomez.vercel.app%2F&text=Link%20to%20share"
-          target="_blank"
-        >
-          <SVG viewBox="0 0 21 18">
-            <g fill="none">
-              <path
-                fill="#d3dae0"
-                d="M0.554,7.092 L19.117,0.078 C19.737,-0.156 20.429,0.156 20.663,0.776 C20.745,0.994 20.763,1.23 20.713,1.457 L17.513,16.059 C17.351,16.799 16.62,17.268 15.88,17.105 C15.696,17.065 15.523,16.987 15.37,16.877 L8.997,12.271 C8.614,11.994 8.527,11.458 8.805,11.074 C8.835,11.033 8.869,10.994 8.905,10.958 L15.458,4.661 C15.594,4.53 15.598,4.313 15.467,4.176 C15.354,4.059 15.174,4.037 15.036,4.125 L6.104,9.795 C5.575,10.131 4.922,10.207 4.329,10.002 L0.577,8.704 C0.13,8.55 -0.107,8.061 0.047,7.614 C0.131,7.374 0.316,7.182 0.554,7.092 Z"
-              ></path>
-            </g>
-          </SVG>
-          TELEGRAM
-        </TelegramButton>
-      </div>
-
-      <p>API BUTTON 2</p>
-      <div style={{ border: "1px solid black", width: 150, cursor: "pointer" }}>
-        <a
-          href="https://xn--r1a.link/share/url?url=https%3A%2F%2Fnextjs-addthis-k70scbjjn-ivgomez.vercel.app%2F&text=Link%20to%20share"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          TELEGRAM
-        </a>
-      </div>
-      <p>API BUTTON 3</p>
-      <div>
-        <a
-          style={{
-            border: "1px solid black",
-            width: 150,
-            cursor: "pointer",
-            display: "block",
-          }}
-          href={`https://t.me/share/url?url=${urlLink}&text=some`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Share
-        </a>
-      </div> */}
-
-      {/* <div
-        className="a2a_kit a2a_kit_size_32 a2a_default_style"
-        data-a2a-url="https://www.corcoran.com/homes-for-sale/474-middle-line-highway-bridgehampton-ny-11932/6418974/regionId=3?WebID=885241"
-        data-a2a-title="Corcoran"
-      >
-        <a className="a2a_button_copy_link" />
-        <a className="a2a_button_whatsapp" />
-        <a className="a2a_button_skype" />
-        <a className="a2a_button_facebook" />
-        <a className="a2a_button_facebook_messenger" />
-        <a className="a2a_button_telegram" />
-      </div> */}
     </div>
   );
 }
 
 export default ShareComponent;
+
+const TiktokShareButton = styled.a`
+  flex-direction: row-reverse;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 1rem 10px 2rem;
+
+  width: 121px;
+  ${breakpoint.tablet` 
+    padding: 0;
+  `}
+
+  span {
+    visibility: hidden;
+  }
+
+  div:nth-child(1) {
+    position: absolute;
+  }
+  div:nth-child(2) {
+    display: flex !important;
+    float: left !important;
+    padding: 0 !important;
+    height: 0 !important;
+    :hover {
+      top: 0 !important;
+      opacity: 1 !important;
+      text-decoration: underline !important;
+      img {
+        background-color: #030303;
+      }
+    }
+  }
+
+  div:nth-child(2):after {
+    content: "TIKTOK";
+    visibility: visible;
+    font-weight: 800;
+    font-size: 12px;
+    padding: 0 1rem !important;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    margin-top: -10px !important;
+    }
+    }
+  }
+
+  img {
+    background-color: #8c8c8c;
+    top: 0 !important;
+    right: 6px !important;
+    height: 22px !important;
+    width: 22px !important;
+    border-radius: 50%;
+    :hover {
+      background-color: #030303;
+    }
+  }
+`;
+
+const ShareThisButton = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 18px 0;
+  :hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+  ${breakpoint.tablet` 
+    padding: 0;
+  `}
+`;
 
 const TelegramButton = styled.a`
   display: block;
