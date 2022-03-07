3503e122b8722fcd24e7fdd3b17d7ffdf106697d
ivgomez <iv.gomez@hotmail.com>
2022-03-07 17:42:08 -0500

    324 img

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <meta
           property="og:image"
-          content="https://mediarouting.vestahub.com/Media/114364984"
+          content="https://mediarouting.vestahub.com/Media/114364984/box/324x324"
         />
         {/* <!-- Go to www.addthis.com/dashboard to customize your tools --> */}
         <link
