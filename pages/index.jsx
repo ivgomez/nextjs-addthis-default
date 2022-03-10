60e8799e6b6ffb23750ae3e780160e2d60bbe982
ivgomez <iv.gomez@hotmail.com>
2022-03-09 19:19:28 -0500

    photo 2 580 restore original image

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <meta
           property="og:image"
-          content="https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/w_500,h_630,c_fill,f_auto/w_580,h_630,c_fill/AgentApi/Realogy/3ACD48C9-0A47-49B2-90D2-BBBE5E695A7E/A1A1EE65-597C-4F5F-A3E0-A749897B3D07.jpg"
+          content="https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/AgentApi/Realogy/3ACD48C9-0A47-49B2-90D2-BBBE5E695A7E/A1A1EE65-597C-4F5F-A3E0-A749897B3D07.jpg"
         />
         <meta property="og:image:width" content="580" />
         <meta property="og:image:height" content="630" />
