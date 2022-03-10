9d1ac9e893c5f05e20db593026b2b88edb33c124
ivgomez <iv.gomez@hotmail.com>
2022-03-09 21:22:03 -0500

    twitter and og image 1024

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,10 +13,10 @@ export default function Home() {
         <meta
           name="twitter:image"
           content="https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/AgentApi/Realogy/3ACD48C9-0A47-49B2-90D2-BBBE5E695A7E/A1A1EE65-597C-4F5F-A3E0-A749897B3D07.jpg"
-        ></meta>
-        {/* <meta name="twitter:card" content="summary" />
-        <meta property="og:image:width" content="200" />
-        <meta property="og:image:height" content="20" /> */}
+        />
+
+        <meta property="og:image:width" content="1024" />
+        <meta property="og:image:height" content="538" />
         {/* <!-- Go to www.addthis.com/dashboard to customize your tools --> */}
         <link
           rel="stylesheet"
