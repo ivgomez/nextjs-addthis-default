b6f0b2de0fbce641b00a30b357c5df9a25241c02
ivgomez <iv.gomez@hotmail.com>
2022-03-16 17:36:06 -0500

    350x350

--- data/data.js
+++ data/data.js
@@ -3,53 +3,53 @@ const data = [
     id: 1,
     pictureName: "Picture 1",
     pictureUrl:
-      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_300,w_350/AgentApi/Realogy/997802F8-8CD0-4C74-A0AA-42EF4B31EFF0/84B3767C-4A67-45E4-ACDB-38F35705C5C6.jpg",
+      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_350,w_350/AgentApi/Realogy/997802F8-8CD0-4C74-A0AA-42EF4B31EFF0/84B3767C-4A67-45E4-ACDB-38F35705C5C6.jpg",
   },
   {
     id: 2,
     pictureName: "Picture 2",
     pictureUrl:
-      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_300,w_350/AgentApi/Realogy/3ACD48C9-0A47-49B2-90D2-BBBE5E695A7E/A1A1EE65-597C-4F5F-A3E0-A749897B3D07.jpg",
+      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_350,w_350/AgentApi/Realogy/3ACD48C9-0A47-49B2-90D2-BBBE5E695A7E/A1A1EE65-597C-4F5F-A3E0-A749897B3D07.jpg",
   },
   {
     id: 3,
     pictureName: "Picture 3",
-    pictureUrl: "https://mediarouting.vestahub.com/Media/114364984/box/340x350",
+    pictureUrl: "https://mediarouting.vestahub.com/Media/114364984/box/350x350",
   },
   {
     id: 4,
     pictureName: "Picture 4",
     pictureUrl:
-      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_300,w_350/AgentApi/Realogy/EF44F2ED-E6B7-4572-90F1-32E8D7BA3C45/DA79718A-1925-4821-B638-CE7D321D6914.jpg",
+      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_350,w_350/AgentApi/Realogy/EF44F2ED-E6B7-4572-90F1-32E8D7BA3C45/DA79718A-1925-4821-B638-CE7D321D6914.jpg",
   },
   {
     id: 5,
     pictureName: "Picture 5",
     pictureUrl:
-      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_300,w_350/AgentApi/Realogy/A3A983A8-C3F1-454A-93F2-997DD3F37CA3/5B6A64F5-AC99-4413-BF28-18DC4FAB4D6C.jpg",
+      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_350,w_350/AgentApi/Realogy/A3A983A8-C3F1-454A-93F2-997DD3F37CA3/5B6A64F5-AC99-4413-BF28-18DC4FAB4D6C.jpg",
   },
   {
     id: 6,
     pictureName: "Picture 6",
     pictureUrl:
-      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_300,w_350/AgentApi/Realogy/36462888-E69C-4F38-BEBB-C8E3E016C597/7FC05352-A76B-454F-98E1-F02794F0FF74.jpg",
+      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_350,w_350/AgentApi/Realogy/36462888-E69C-4F38-BEBB-C8E3E016C597/7FC05352-A76B-454F-98E1-F02794F0FF74.jpg",
   },
   {
     id: 7,
     pictureName: "Picture 7",
     pictureUrl:
-      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_300,w_350/v1646346614/AgentApi/Realogy/15F2D7FE-61A6-46A1-96A7-27214E4B1915/02F79DCD-8146-40BA-950D-9BB9BFB96C77.jpg",
+      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_350,w_350/v1646346614/AgentApi/Realogy/15F2D7FE-61A6-46A1-96A7-27214E4B1915/02F79DCD-8146-40BA-950D-9BB9BFB96C77.jpg",
   },
   {
     id: 8,
     pictureName: "Picture 8",
     pictureUrl:
-      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_300,w_350/AgentApi/Realogy/D5037B2E-3B6F-4829-822F-6357A8776878/9A40CC99-6360-4C1E-929E-53A3DD78B369.jpg",
+      "https://res.cloudinary.com/corcoran-dryrmqrbg/image/upload/c_fill,g_face,h_350,w_350/AgentApi/Realogy/D5037B2E-3B6F-4829-822F-6357A8776878/9A40CC99-6360-4C1E-929E-53A3DD78B369.jpg",
   },
   {
     id: 9,
     pictureName: "Picture 9",
-    pictureUrl: "https://mediarouting.vestahub.com/Media/98021925/box/340x350",
+    pictureUrl: "https://mediarouting.vestahub.com/Media/98021925/box/350x350",
   },
 ];
 
