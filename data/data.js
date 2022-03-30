bb095dfa50b122a7a392af473bc4c501dc67f497
ivgomez <iv.gomez@hotmail.com>
2022-03-30 11:42:53 -0500

    set 700px + 65p + new phts

--- data/data.js
+++ data/data.js
@@ -9,35 +9,38 @@ const data = [
     id: 2,
     pictureName: "Picture 2",
     pictureUrl:
-      "https://dhpt9nfi8o3qa.cloudfront.net/eyJidWNrZXQiOiJjb3Jjb3Jhbi1kZXYtaW1hZ2UtcHJvY2Vzc2luZyIsImtleSI6IkFnZW50QXBpL1JlYWxvZ3kvQkQ5QzA4RDktMEI1OS00QjdBLTlGNkEtMEIzREM1RDBFODQ0Lzc0RTMxNjgyLURDNEItNDlBNS1CMDJBLUQ5NkRDRTkxMzBBMyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6NzAwfSwic21hcnRDcm9wIjp7ImZhY2VJbmRleCI6MCwicGFkZGluZyI6NjB9fX0=",
+      "https://dhpt9nfi8o3qa.cloudfront.net/eyJidWNrZXQiOiJjb3Jjb3Jhbi1kZXYtaW1hZ2UtcHJvY2Vzc2luZyIsImtleSI6IkFnZW50QXBpL1JlYWxvZ3kvMzY0NjI4ODgtRTY5Qy00RjM4LUJFQkItQzhFM0UwMTZDNTk3LzdGQzA1MzUyLUE3NkItNDU0Ri05OEUxLUYwMjc5NEYwRkY3NCIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6NzAwfSwic21hcnRDcm9wIjp7ImZhY2VJbmRleCI6MCwicGFkZGluZyI6NjV9fX0=",
   },
   {
     id: 3,
     pictureName: "Picture 3",
     pictureUrl:
-      "https://dhpt9nfi8o3qa.cloudfront.net/eyJidWNrZXQiOiJjb3Jjb3Jhbi1kZXYtaW1hZ2UtcHJvY2Vzc2luZyIsImtleSI6IkFnZW50QXBpL1JlYWxvZ3kvMzY0NjI4ODgtRTY5Qy00RjM4LUJFQkItQzhFM0UwMTZDNTk3LzdGQzA1MzUyLUE3NkItNDU0Ri05OEUxLUYwMjc5NEYwRkY3NCIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6NzAwfSwic21hcnRDcm9wIjp7ImZhY2VJbmRleCI6MCwicGFkZGluZyI6NjB9fX0=",
+      "https://dhpt9nfi8o3qa.cloudfront.net/eyJidWNrZXQiOiJjb3Jjb3Jhbi1kZXYtaW1hZ2UtcHJvY2Vzc2luZyIsImtleSI6IkFnZW50QXBpL1JlYWxvZ3kvQkQ5QzA4RDktMEI1OS00QjdBLTlGNkEtMEIzREM1RDBFODQ0Lzc0RTMxNjgyLURDNEItNDlBNS1CMDJBLUQ5NkRDRTkxMzBBMyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6NzAwfSwic21hcnRDcm9wIjp7ImZhY2VJbmRleCI6MCwicGFkZGluZyI6NjV9fX0=",
   },
   {
     id: 4,
     pictureName: "Picture 4",
     pictureUrl:
-      "https://dhpt9nfi8o3qa.cloudfront.net/eyJidWNrZXQiOiJjb3Jjb3Jhbi1kZXYtaW1hZ2UtcHJvY2Vzc2luZyIsImtleSI6IkFnZW50QXBpL1JlYWxvZ3kvOTk3ODAyRjgtOENEMC00Qzc0LUEwQUEtNDJFRjRCMzFFRkYwLzg0QjM3NjdDLTRBNjctNDVFNC1BQ0RCLTM4RjM1NzA1QzVDNiIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6NzAwfSwic21hcnRDcm9wIjp7ImZhY2VJbmRleCI6MCwicGFkZGluZyI6NjB9fX0=",
+      "https://dhpt9nfi8o3qa.cloudfront.net/eyJidWNrZXQiOiJjb3Jjb3Jhbi1kZXYtaW1hZ2UtcHJvY2Vzc2luZyIsImtleSI6IkFnZW50QXBpL1JlYWxvZ3kvMTVGMkQ3RkUtNjFBNi00NkExLTk2QTctMjcyMTRFNEIxOTE1LzAyRjc5RENELTgxNDYtNDBCQS05NTBELTlCQjlCRkI5NkM3NyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6NzAwfSwic21hcnRDcm9wIjp7ImZhY2VJbmRleCI6MCwicGFkZGluZyI6NjV9fX0=",
   },
-  // {
-  //   id: 5,
-  //   pictureName: "Picture 5",
-  //   pictureUrl: "",
-  // },
-  // {
-  //   id: 6,
-  //   pictureName: "Picture 6",
-  //   pictureUrl: "",
-  // },
-  // {
-  //   id: 7,
-  //   pictureName: "Picture 7",
-  //   pictureUrl: "",
-  // },
+  {
+    id: 5,
+    pictureName: "Picture 5",
+    pictureUrl:
+      "https://dhpt9nfi8o3qa.cloudfront.net/eyJidWNrZXQiOiJjb3Jjb3Jhbi1kZXYtaW1hZ2UtcHJvY2Vzc2luZyIsImtleSI6IkFnZW50QXBpL1JlYWxvZ3kvODAyRTY1RjgtMzNGRS00QzI3LTlCQUUtMTY1RDI2NjM0QjdFL0JDMDJBQkNCLTQzRjYtNDU3NC1BRDgwLTU3MjcwRjRBRTNDNCIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6NzAwfSwic21hcnRDcm9wIjp7ImZhY2VJbmRleCI6MCwicGFkZGluZyI6NjV9fX0=",
+  },
+  {
+    id: 6,
+    pictureName: "Picture 6",
+    pictureUrl:
+      "https://dhpt9nfi8o3qa.cloudfront.net/eyJidWNrZXQiOiJjb3Jjb3Jhbi1kZXYtaW1hZ2UtcHJvY2Vzc2luZyIsImtleSI6IkFnZW50QXBpL1JlYWxvZ3kvQTNBOTgzQTgtQzNGMS00NTRBLTkzRjItOTk3REQzRjM3Q0EzLzVCNkE2NEY1LUFDOTktNDQxMy1CRjI4LTE4REM0RkFCNEQ2QyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6NzAwfSwic21hcnRDcm9wIjp7ImZhY2VJbmRleCI6MCwicGFkZGluZyI6NjV9fX0=",
+  },
+  {
+    id: 7,
+    pictureName: "Picture 7",
+    pictureUrl:
+      "https://dhpt9nfi8o3qa.cloudfront.net/eyJidWNrZXQiOiJjb3Jjb3Jhbi1kZXYtaW1hZ2UtcHJvY2Vzc2luZyIsImtleSI6IkFnZW50QXBpL1JlYWxvZ3kvRDUwMzdCMkUtM0I2Ri00ODI5LTgyMkYtNjM1N0E4Nzc2ODc4LzlBNDBDQzk5LTYzNjAtNEMxRS05MjlFLTUzQTNERDc4QjM2OSIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6NzAwfSwic21hcnRDcm9wIjp7ImZhY2VJbmRleCI6MCwicGFkZGluZyI6NjV9fX0=",
+  },
   // {
   //   id: 8,
   //   pictureName: "Picture 8",
