46b15488dfe35dec3bbb012ad4ab026620531a6e
ivgomez <iv.gomez@hotmail.com>
2022-03-30 08:41:38 -0500

    set aws api server

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["res.cloudinary.com", "mediarouting.vestahub.com"],
+    domains: [
+      "res.cloudinary.com",
+      "mediarouting.vestahub.com",
+      "dhpt9nfi8o3qa.cloudfront.net",
+    ],
   },
   //pageExtensions: ["index.tsx", "index.ts", "index.jsx", "index.js"],
 };
